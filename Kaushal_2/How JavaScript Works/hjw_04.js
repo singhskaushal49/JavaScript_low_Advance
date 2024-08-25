114ae0c8d3621b4704f42ca1c7ad7a8cc558703b
Kaushlendra Pratap Singh <singhskaushal49@gmail.com>
2024-08-25 23:06:36 +0600

    25-08-2024

---
+++ Kaushal_2/How JavaScript Works/hjw_04.js
@@ -0,0 +1,17 @@
+// Uncaught ReferenceError:
+// Connot access 'firstName' before initialization
+
+
+
+// Uncaught ReferenceError:
+// firstName is not defined
+
+// console.log(firstName);
+
+// console.log(firstName);
+// let firstName;
+// console.log(firstName);
+
+// console.log(type of firstName);
+
+// let firstName = "Kaushal"
---
+++ Kaushal_2/How JavaScript Works/hjw_05.js
@@ -0,0 +1,10 @@
+console.log("hello world");
+let firstName = "Kaushal";
+let lastName = "Singh";
+
+const myFunction = function() {
+  let var1 = "First Variable";
+  let var2 = "Second Variable";
+  console.log(val1);
+  console.log(var2);
+}
