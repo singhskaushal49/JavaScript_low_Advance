444cf1df42cc0ae05db84cc04035c754f5f5576a
Kaushlendra Pratap Singh <singhskaushal49@gmail.com>
2024-09-07 00:18:05 +0600

    07-09-2024

---
+++ Kaushal_2/Dom Tutorial/Lec108.js
@@ -0,0 +1,7 @@
+// get multiple elements using getElements by class name
+// get multiple elements item using querySelectorAll
+// const navItems = document.getElementsByClassName("nav-item"); // HTMLCollection
+// console.log(navItems);
+// console.log(Array.isArray(navItem));
+// const navItems = document.querySelectorAll(".nav-item"); // NodeList
+// console.log(navItems[1]);
---
+++ Kaushal_2/Dom Tutorial/Lec110.js
@@ -0,0 +1,6 @@
+// innerHtml
+const headline = document.querySelector(".headline");
+// console.log(headline.innerHTML);
+// headline.innerHTML = "<h1> Inner html changed </h1>";
+// headline.innerHTML += "<button class= \"btn\"> Learn More </button>"
+// console.log(headline.innerHTML);
