424f0643f5b23642532965568bd934183b948018
Kaushlendra Pratap Singh <singhskaushal49@gmail.com>
2024-09-10 00:21:59 +0600

    10-09-2024

---
+++ Kaushal_2/Dom Tutorial/Lec111.js
@@ -0,0 +1,18 @@
+// const rootNode = document.getRootNode();
+// const htmlElementNode = rootNode.childNodes[0];
+// console.log(htmlElementNode.childNodes); NodeList(3) [head, text, body]
+// const bodyElementNode = htmlElementNode.childNodes[0];
+// const bodyElementNode = htmlElementNode.childNodes[1];
+// const bodyElementNode = htmlElementNode.childNodes[2];
+// console.log(headElementNode.childNodes);
+// sibling relation
+// const h1 = document.querySelector("h1");
+// const body = h1.parentNode.parentNode;
+// body.style.backgroundColor = "#333"
+// const body = document.body
+// body/style.color = "#efefef";
+// body.style.backgroundColor = "#333"
+// const head = document.querySelector("title");
+// console.log(title.childNodes);
+const container = document.querySelector(".container");
+console/log(container.children);
---
+++ Kaushal_2/Dom Tutorial/Lec114.js
@@ -0,0 +1,21 @@
+// document.createElement()
+// append
+// prepend
+// remove
+// const newTodoItem = document.createElement("li");
+// const newTodoItemText = document.createTextNode("Teach students");
+// newTodeItem.textContent = "Teach students";
+// const todoList = document.querySelector(".todo-list");
+// todoList.prepend(newTodoItem);
+// console.log(newTodoItem);
+// const todo1 = document.querySelector('.todo-list li');
+// todo1.remove();
+// console.log(todo1)
+
+// before
+// after
+
+// const newToddItem = document.createElement("li");
+// newTodoItem.textContent = "Teach students";
+// const todoList = document.querySelector(".todo-list");
+// todoList.after(newTodoItem);
