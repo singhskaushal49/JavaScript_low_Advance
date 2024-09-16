f344107cf6ef07b09c9730bdb8b76b259f72f238
Kaushlendra Pratap Singh <singhskaushal49@gmail.com>
2024-09-17 01:07:25 +0600

    17-09-2024

---
+++ Kaushal_2/Dom Tutorial/Lec128.js
@@ -0,0 +1,21 @@
+// console.log("hello world");
+
+const grandparent = document.querySelector(".grandparent");
+// const parent = document.querySelector(".parent");
+// const child = document.querySelector(".child");
+
+// capturing events
+// child.addEventListener(
+//   "click",
+//   () => {
+//      console.log("capture !!!! child");
+//   },
+//  true
+// );
+//  parent.addEventListener(
+//  "click",
+//  () => {
+//     console.log("capture !!!! grandparent");
+//  },
+//  true
+// );
